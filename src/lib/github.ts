358b2f892ec27af66ea1f904178b52936d0f9718
sudatra <mail2sudatra@gmail.com>
2025-02-02 18:41:06 +0530

    fixed typo in commit create

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -29,7 +29,7 @@ export const getCommitHashes = async (githubUrl: string): Promise<Response[]> =>
   const sortedCommits = data.sort((a: any, b: any) => (new Date(b.commit.author?.date).getTime() - new Date(a.commit.author?.date).getTime())) as any[]
   return sortedCommits.slice(0, 15).map((commit: any) => ({
     commitHash: commit.sha as string,
-    commitMessage: commit.commit.mesage ?? '',
+    commitMessage: commit.commit.message ?? '',
     commitAuthorName: commit.commit?.author?.name ?? '',
     commitAuthorAvatar: commit?.author?.avatar_url ?? '',
     commitDate: commit.commit?.author?.date ?? ''
@@ -53,7 +53,6 @@ export const pollCommits = async (projectId: string) => {
   });
 
   const commits = await db.commit.createMany({
-    
     data: commitSummaries.map((summary: any, index) => {
       return {
         projectId: projectId,
