ba2f1d98d65f17275f2974bffb00215199414d29
sudatra <mail2sudatra@gmail.com>
2025-02-06 21:28:17 +0530

    added questions fetch trpc query

--- src/app/(protected)/qa/page.tsx
+++ src/app/(protected)/qa/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+
+import { Sheet } from '@/components/ui/sheet';
+import useProject from '@/hooks/use-project'
+import { api } from '@/trpc/react';
 import React from 'react'
 
 const QAPage = () => {
+  const { projectId } = useProject();
+  const { data: questions } = api.project.getQuestions.useQuery({ projectId })
+
   return (
-    <div>
-      QAPage
-    </div>
+    <Sheet>
+      
+    </Sheet>
   )
 }
 
