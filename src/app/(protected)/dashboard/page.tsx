55d7da4d85faab3adce359fead52d98e6a3f0aab
sudatra <mail2sudatra@gmail.com>
2025-02-09 19:41:48 +0530

    added team members display ui

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -9,6 +9,7 @@ import AskQuestionCard from './ask-question-card';
 import MeetingCard from './meeting-card';
 import ArchiveButton from './archive-button';
 import InviteButton from './invite-button';
+import TeamMembers from './team-members';
 
 const DashboardPage = () => {
   const { project } = useProject();
@@ -37,7 +38,7 @@ const DashboardPage = () => {
 
         <div className='h-4' />
         <div className='flex items-center gap-4'>
-          TeamMembers
+          <TeamMembers />
           <InviteButton />
           <ArchiveButton />
         </div>
