6f4cd78a4cf515b5a13913ca114dd0c11606e62c
sudatra <mail2sudatra@gmail.com>
2025-02-10 21:10:20 +0530

    added ui for billings page, redirect to stripe checkout page

--- src/app/(protected)/billing/page.tsx
+++ src/app/(protected)/billing/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+'use client'
+
+import { Button } from '@/components/ui/button';
+import { Slider } from '@/components/ui/slider';
+import { createCheckoutSession } from '@/lib/stripe';
+import { api } from '@/trpc/react'
+import { Info } from 'lucide-react';
+import React, { useState } from 'react'
 
 const BillingPage = () => {
+  const { data: user } = api.project.getUserCredits.useQuery();
+  const [creditsToBuy, setCreditsToBuy] = useState<number[]>([100]);
+  const creditsToBuyAmount = creditsToBuy[0]!;
+  const price = (creditsToBuyAmount / 50).toFixed(2);
+
   return (
     <div>
-      BillingPage
+      <h1 className='text-xl font-semibold'>Billing</h1>
+      <div className='h-2' />
+      <p className='text-sm text-gray-500'>You currently have {user?.credits} credits</p>
+      <div className='h-2' />
+      <div className='bg-blue-50 px-4 py-2 rounded-md border border-blue-200 text-blue-700'>
+        <div className='flex items-center gap-2'>
+          <Info className='size-4' />
+          <p>Each credit allows Indexing 1 file in a repository</p>
+        </div>
+
+        <p className='text-sm'>Eg. If your Project has 100 files, 100 credits will be utilized for indexing</p>
+      </div>
+
+      <div className='h-4' />
+      <Slider 
+        defaultValue={[100]}
+        max={1000}
+        min={10}
+        step={10}
+        value={creditsToBuy}
+        onValueChange={(value) => setCreditsToBuy(value)}
+      />
+
+      <div className='h-4' />
+      <Button onClick={() => createCheckoutSession(creditsToBuyAmount)}>
+        Buy {creditsToBuyAmount} for ${price}
+      </Button>
     </div>
   )
 }
--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -161,5 +161,12 @@ export const projectRouter = createTRPCRouter({
       where: { projectId: input.projectId },
       include: { user: true }
     })
+  }),
+
+  getUserCredits: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.user.findUnique({
+      where: { id: ctx.user.userId! },
+      select: { credits: true }
+    })
   })
 })
